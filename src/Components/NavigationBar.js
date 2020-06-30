b87f2c4635852c0a1658c55bd80ff649787fb7c2
TavsMalling <67231672+TavsMalling@users.noreply.github.com>
2020-06-30 23:24:13 +0200

    rf

---
+++ src/Components/NavigationBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import NavbarBrand from "react-bootstrap/NavbarBrand";
+import Nav from "react-bootstrap/Nav";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+
+function navBar() {
+  return <Button className="btn btn-secondary">HelloSS</Button>;
+}
+
+export default navBar;
