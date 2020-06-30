2df1e665d72970f5b0d8c19e0cde157dde79c26f
TavsMalling <67231672+TavsMalling@users.noreply.github.com>
2020-06-30 23:34:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import NavBar from "./Components/NavBar";
 import Footer from "./Components/Footer";
-import Image from "react-bootstrap/Image";
-import GitHubLogo from "./Assets/Images/github-logo.png";
 
 function App() {
   return (
