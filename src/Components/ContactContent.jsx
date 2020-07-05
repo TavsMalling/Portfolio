f5ad37e3567733a4b8da7b0ba23c069bfdbb36a2
TavsMalling <67231672+TavsMalling@users.noreply.github.com>
2020-07-05 22:20:24 +0200

    Final update

--- src/Components/ContactContent.jsx
+++ src/Components/ContactContent.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
+import ContactForm from "./ContactForm";
 
 function contactContent() {
-    return (
-        <div>
-            This is the Contact section!
-        </div>
-
-    );
+  return (
+    <Container className="text-center">
+      <Row>
+        <Col>
+          <p>Phone: +45 71 84 79 26</p>
+          <p>Email: Tavsmalling@gmail.com</p>
+          <p>Linkedin: www.linkedin.com/in/tavs-malling-565933192</p>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
-
+//<ContactForm />
 export default contactContent;
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,35 +2,37 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Link from "react-router-dom/Link";
 
-import '../Assets/Styles/Footer.css'
+import "../Assets/Styles/Footer.css";
 
 function Footer() {
-    return (
-        <footer className="mt-5 mx-5">
-            <Container fluid>
-                <Row className="border-top p-1 justify-content-between" >
-                    <Col className="p-0" md={3} sm={12}>
-                        <p class='FooterText LeftText' >Tavs Malling</p>
-                    </Col>
-                    <Col className="p-0 my-3" md={3} sm={12}>
-                        <Row className="p-0 justify-content-center" >
-                            <h1 className="font-weight-bolder">Interested?</h1>
-                        </Row>
-                        <Row
-                            className="p-0 border-top justify-content-center">
-                            <button className="btn btn-primary btn-sm mt-3">
-                                Contact Me
-                            </button>
-                        </Row>
-                    </Col>
-                    <Col className="p-0" md={3} sm={12}>
-                        <p class='FooterText RightText' >This site was made with React.js.</p>
-                    </Col>
-                </Row>
-            </Container>
-        </footer>
-    );
+  return (
+    <footer className="mt-5 mx-5">
+      <Container fluid>
+        <Row className="border-top p-1 justify-content-between">
+          <Col className="p-0" md={3} sm={12}>
+            <p class="FooterText LeftText">Tavs Malling</p>
+          </Col>
+          <Col className="p-0 my-3" md={3} sm={12}>
+            <Row className="p-0 justify-content-center">
+              <h1 className="font-weight-bolder">Interested?</h1>
+            </Row>
+            <Row className="p-0 border-top justify-content-center">
+              <Link className="mr-1 my-auto" to="/Contact">
+                Contact me
+              </Link>
+            </Row>
+          </Col>
+          <Col className="p-0" md={3} sm={12}>
+            <p class="FooterText RightText">
+              This site was made with React.js.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 }
 
 export default Footer;
